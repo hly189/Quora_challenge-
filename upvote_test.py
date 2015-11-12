a443c597a78c06b3b519fc50c3dc73b10ed823f3
Hoa Ly <lythaihoa2311992@gmail.com>
2015-11-11 20:24:21 -0800

    Quora Challenge

--- upvote_test.py
+++ upvote_test.py
@@ -12,20 +12,21 @@ def subrange(upvote):
 		previous = upvote[i-1]
 		current = upvote[i]
 		
-		if current > previous: 
-		    if count_decreasing == 0:
-		        count_decreasing = 1
-		    else: 
-		    	count_decreasing = count_decreasing + 1
-		    non_decreasing = non_decreasing + count_decreasing 
-		    count_increasing = 0
-		elif current < previous: 
+		if current < previous: 
 		    if count_increasing == 0:
 		        count_increasing = 1
 		    else: 
 		    	count_increasing = count_increasing + 1
 		    non_increasing = non_increasing + count_increasing 
 		    count_decreasing = 0
+		elif current > previous: 
+		    if count_decreasing == 0:
+		        count_decreasing = 1
+		    else: 
+		    	count_decreasing = count_decreasing + 1
+		    non_decreasing = non_decreasing + count_decreasing 
+		    count_increasing = 0
+		
 		else: 
 			if count_increasing ==0: 
 				count_increasing = 1
